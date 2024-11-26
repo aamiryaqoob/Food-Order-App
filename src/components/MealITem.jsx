45cfb5f15cb52ee123c491b3df6d4f263fb8da2a
aamiryaqoob <aamiryaqoob965@gmail.com>
2024-11-26 21:47:27 +0500

    MealItem.jsx add

--- src/components/MealITem.jsx
+++ src/components/MealITem.jsx
@@ -1,8 +1,12 @@
-export default function MealItem({meal}) {
+export default function MealItem({ meal }) {
     return <li className="meal-item">
         <article>
-        <img src={meal.image} alt="Meal Image" />
-        
+            <img src={`http://localhost:3000/${meal.image}`} alt="Meal Image" />
+            <div className="meal-item-title">
+                <h3>{meal.name}</h3>
+                <p>{meal.description}</p>
+            </div>
+            <p>{meal.price}</p>
         </article>
     </li>
 }
