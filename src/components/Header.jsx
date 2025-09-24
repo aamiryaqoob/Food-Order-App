4fd293270abcad638c08d7d07c2e7ec73c5642a0
aamiryaqoob <aamiryaqoob965@gmail.com>
2025-09-24 19:15:43 +0500

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Logo from '../assets/logo.jpg';
+import React from "react";
+import Logo from "../assets/logo.jpg";
 
 const Header = () => {
-    return (
-        <header id='main-header'>
-            <div id='title'>
-                <img src={Logo} alt="Logo" />
-                <h1>React Food Order</h1>
-            </div>
-            <nav>
-                <button>Cart(0)</button>
-            </nav>
-        </header>
-    )
-}
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={Logo} alt="Logo" />
+        <h1>React Food Order</h1>
+      </div>
+      <nav>
+        <button>Cart(0)</button>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
